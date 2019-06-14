556977327194e58278c888c1958f2e8158a289a2
Reed James <reedtjames1234@gmail.com>
2019-06-14 11:30:09 -0500

    MVP complete

--- village/src/App.js
+++ village/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
+import { Route, NavLink, withRouter } from "react-router-dom";
 import SmurfForm from "./components/SmurfForm";
 import Smurfs from "./components/Smurfs";
 import { get } from "http";
@@ -40,11 +41,31 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurf={this.addSmurf} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <nav className="nav">
+          <h1>Smurf Nav</h1>
+          <div>
+            <NavLink className="navLink" exact to="/">
+              SmurfList
+            </NavLink>
+            <NavLink exact to="/smurf-form">
+              Smurfform
+            </NavLink>
+          </div>
+        </nav>
+
+        <Route
+          exact
+          path="/"
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
+        <Route
+          exact
+          path="/smurf-form"
+          render={props => <SmurfForm {...props} addSmurf={this.addSmurf} />}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
